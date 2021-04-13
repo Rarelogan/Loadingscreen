ed849047a97a4c0e2410ccd114947b03a02d7045
Rarelogan <82425856+Rarelogan@users.noreply.github.com>
2021-04-12 20:46:30 -0700

    loadingscreentest

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+
+//General
+$APIKey = "028C4435CA17CC9E7823F6EDB600DF6B"; // Go get your API here: http://steamcommunity.com/dev/apikey
+
+$load_name ="Redemption Networks"; // For your Browser Name
+$community_name ="Redemption Networks"; // Your community/server name. The one in the middle
+$community_url = "https://redemptionnetworks11.mistforums.com"; // Url for your users to know where to go
+$intro = TRUE; // If you want to turn this off, type FALSE instead
+// Introduction
+$tagline = " A long time ago, on a Star Destroyer,<br> far away...."; // The intro text in the beginning of the animation
+// Above Server Name
+$sub_title_1 = "Welcome to the force"; // Above your server name
+// Under Server Name
+$sub_title_2 = "read the rules to learn the ways of the force"; // Below your server name
+
+//Bottom Part
+$left_bottom_title = "Discord";
+$left_bottom_sub_title = "https://discord.gg/HA6X4mEfFd";
+$right_bottom_title = "Our Server";
+$right_bottom_sub_title = "General information about the server";
+
+$left_bottom_text = "Redemption Networks is a brand new Imperial RP server. We are actively looking for Staff and Commanders. You can apply on our discord. We hope you enjoy your stay.";
+
+?>
